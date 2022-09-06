52b62c1514222e9a330d08a2ab6f5ebc93cc8544
HJP <HJP@qq.com>
2022-09-06 22:03:33 +0800

    校园外卖1.0

---
+++ src/main/java/com/hjp/xywm/XywmApplication.java
@@ -0,0 +1,15 @@
+package com.hjp.xywm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Slf4j
+@SpringBootApplication
+public class XywmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XywmApplication.class, args);
+        log.info("项目启动成功");
+    }
+
+}
---
+++ src/test/java/com/hjp/xywm/XywmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjp.xywm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XywmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
